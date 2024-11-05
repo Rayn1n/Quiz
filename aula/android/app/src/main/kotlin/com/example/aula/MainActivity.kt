0a081034fd377aebe0059563fde1dd21657a20ee
netox <miguelneto0708@gmail.com>
2024-11-05 09:02:22 -0300

    V 1.1

--- aula/android/app/src/main/kotlin/com/example/aula/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.aula
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- flutter_application_lista_de_tarefas/android/app/src/main/kotlin/com/example/flutter_application_lista_de_tarefas/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_application_lista_de_tarefas
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
